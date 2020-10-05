7fb7ac2877d6f1d7c27f7384f8827498388b5a94
Christian <mbappec@gmail.com>
2020-10-05 21:34:11 +0100

    completed

---
+++ parsingjson.js
@@ -0,0 +1,18 @@
+$(function() {
+    $("#send").click(function() {
+        const userId = $("#useriId").val();
+        $.get("http://jsonplaceholder.typicode.com/posts", { "data": { "userId": userId } })
+            .done(function(result) {
+                $.each(result, function(index, val) {
+                    $("#result").append("<h2>No:" + val.id + " </h2>")
+                    $("#result").append("<h5>Title: " + val.title + " </h5>")
+                    $("#result").append("<p><b>post: </b>" + val.body + " </p>")
+                })
+            })
+            .fail(function() {
+
+
+            })
+
+    })
+});
